48949034475a2ad606c157d54f9a16932b2330fd
Golden-021 <ab4630714@gmail.com>
2025-07-07 15:49:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   // Auto Image Slider for Hero Section
   const hero = document.querySelector('.hero');
   const images = [
-    'https://source.unsplash.com/random/1920x1080?nature',
+    'https://campaginimages.s3.ap-south-1.amazonaws.com/target002.png',
     'https://source.unsplash.com/random/1920x1080?city',
     'https://source.unsplash.com/random/1920x1080?technology',
     'https://source.unsplash.com/random/1920x1080?business'
